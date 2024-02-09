9dc8218d6fd9060db48a84b36da1c46be80cd844
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-08 18:57:29 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function insertionSortReverse(array) {
-    return array;
+    for (let i = array.length - 2; i >= 0; i--) {
+        let key = array[i];
+        let j = i + 1;
+        while (j <= array.length - 1 && key > array[j]) {
+            array[j - 1] = array[j];
+            j++;
+        }
+        array[j - 1] = key;
+    }
 }
+
+module.exports = { insertionSortReverse };
+
